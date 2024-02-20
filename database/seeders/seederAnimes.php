f133e58bf480820575e93bf4f73952af82da6d40
guillermobastos <bastosribasguillermo@gmail.com>
2024-02-20 11:04:10 +0100

    tercer commit

--- database/seeders/seederAnimes.php
+++ database/seeders/seederAnimes.php
@@ -18,23 +18,33 @@ class seederAnimes extends Seeder
     {
         DB::table('animes')->insert([
             [
+                'categoria_id' => 1,
                 'nombreAnime' => 'One punch',
                 'duracion' => 55,
                 'descripcion' => 'Hombre super fuerte',
                 'valoracion' => 7.5,
             ],
             [
+                'categoria_id' => 2,
                 'nombreAnime' => 'Hunter x Hunter',
                 'duracion' => 78,
                 'descripcion' => 'Aventuras de un niño',
                 'valoracion' => 8.9,
             ],
             [
+                'categoria_id' => 3,
                 'nombreAnime' => 'One Piece',
                 'duracion' => 1100,
                 'descripcion' => 'Hombre de goma',
                 'valoracion' => 9.4,
             ]
         ]);
+
+        DB::table('users')->insert([
+                'name' => 'guillermo',
+                'email' => 'asdf@gmail.com',
+                'password' => 'guillermo123',
+        ]);
+
     }
 }
